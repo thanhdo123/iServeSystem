d8650dcda1e2c08d721daeef1abc71791282c304
thanhdo <thanhdobk99check02@gmail.com>
2015-05-27 09:46:16 +0700

    menu

---
+++ IServeSystem/app/src/main/java/com/i/serve/iservesystem/dto/MenuItem.java
@@ -0,0 +1,7 @@
+package com.i.serve.iservesystem.dto;
+
+/**
+ * Created by Admin on 5/27/2015.
+ */
+public class MenuItem {
+}
---
+++ IServeSystem/app/src/main/java/com/i/serve/iservesystem/service/MenuService.java
@@ -0,0 +1,8 @@
+package com.i.serve.iservesystem.service;
+
+/**
+ * Created by Admin on 5/27/2015.
+ */
+public class MenuService {
+
+}
